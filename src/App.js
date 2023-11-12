16233f86a471ec3f77b04eead29c2a369d186386
Ztaclie <melmeh42@gmail.com>
2023-11-12 21:10:58 +0300

    hotfix

--- src/App.js
+++ src/App.js
@@ -4,53 +4,7 @@ import ContactMe from "./components/ContactMe";
 import "./css/App.css";
 
 function App() {
-  function StrToLan(inputString) {
-    const parts = inputString.split("|");
-
-    // Check if the split parts array has two parts for the left and right side of '|'
-    if (parts.length === 2) {
-      // Get the browser's language
-      const language = navigator.language || navigator.userLanguage;
-
-      // Check if the browser's language is Turkish
-      if (language === "tr" || language.startsWith("tr-")) {
-        // Return the right side of '|'
-        return parts[1];
-      } else {
-        // Return the left side of '|'
-        return parts[0];
-      }
-    } else {
-      // If input does not contain '|', return the whole string or an error message
-      return inputString || 'Input string does not contain "|" character';
-    }
-  }
-
-  return (
-    <div className="App">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12">
-            <Hero />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <Skills
-              StrToLan={StrToLan}
-              title={StrToLan("My Expertise|Benim Yeteneklerim")}
-              content={StrToLan("ENGLISH CONTENT|TURKISH CONTENT")}
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <ContactMe StrToLan={StrToLan} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
