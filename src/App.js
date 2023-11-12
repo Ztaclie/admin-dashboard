95634aac491372e34d1b77d240e0072b1ef53fa3
Ztaclie <melmeh42@gmail.com>
2023-11-12 21:09:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import ContactMe from "./components/ContactMe";
+import "./css/App.css";
 
 function App() {
+  function StrToLan(inputString) {
+    const parts = inputString.split("|");
+
+    // Check if the split parts array has two parts for the left and right side of '|'
+    if (parts.length === 2) {
+      // Get the browser's language
+      const language = navigator.language || navigator.userLanguage;
+
+      // Check if the browser's language is Turkish
+      if (language === "tr" || language.startsWith("tr-")) {
+        // Return the right side of '|'
+        return parts[1];
+      } else {
+        // Return the left side of '|'
+        return parts[0];
+      }
+    } else {
+      // If input does not contain '|', return the whole string or an error message
+      return inputString || 'Input string does not contain "|" character';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <Hero />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Skills
+              StrToLan={StrToLan}
+              title={StrToLan("My Expertise|Benim Yeteneklerim")}
+              content={StrToLan("ENGLISH CONTENT|TURKISH CONTENT")}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <ContactMe StrToLan={StrToLan} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
